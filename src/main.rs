53ffccbdcf0d39f5ae97c80155290d6622529386
Thvarzil <louisblebohec@gmail.com>
2019-09-09 12:31:33 -0600

    progress commit - working through compilation issues

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::str::FromStr;
+
+fn parse(){
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the roll you are looking for:");
+    println!("Example: 2d6");
+
+    let mut desired_roll = String::new();
+
+    io::stdin().read_line(&mut desired_roll)
+        .expect("Failed to read input");
+
+    let mut desired_roll: Vec<&str> = desired_roll.split("d").collect();
+    desired_roll[0] = desired_roll[0].trim().parse().unwrap();
+    desired_roll[1] = desired_roll[1].trim().parse().unwrap();
+    assert_eq!(desired_roll, [2,6]);
+    //let v: Vec<&str> = "Mary had a little lamb".split(' ').collect();
+
+    println!("{} {}", desired_roll[0], desired_roll[1]);
 }
