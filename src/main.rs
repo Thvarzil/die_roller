5cf043fa84174046666bd24dbc318b8967c23811
Thvarzil <louisblebohec@gmail.com>
2019-09-09 15:25:57 -0600

    Added comments, working on RNG.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,63 @@
 use std::io;
-use std::str::FromStr;
+use std::cmp::Ordering;
+use rand::Rng;
 
-fn parse(){
-
-}
 
 fn main() {
     println!("Enter the roll you are looking for:");
     println!("Example: 2d6");
 
+    //Initializing string to store user input
     let mut desired_roll = String::new();
 
+    //Retrieving user input from CLI
     io::stdin().read_line(&mut desired_roll)
         .expect("Failed to read input");
 
-    let mut desired_roll: Vec<&str> = desired_roll.split("d").collect();
-    desired_roll[0] = desired_roll[0].trim().parse().unwrap();
-    desired_roll[1] = desired_roll[1].trim().parse().unwrap();
-    assert_eq!(desired_roll, [2,6]);
-    //let v: Vec<&str> = "Mary had a little lamb".split(' ').collect();
+    //Splitting user input into the two relevant parts - the number and type of the dice
+    let desired_roll: Vec<&str> = desired_roll.split("d").collect();
+    //Parsing each part into a number and storing in separate variables
+    let iterator= desired_roll[0].trim().parse::<i32>().unwrap();
+    let die = desired_roll[1].trim().parse::<i32>().unwrap();
 
-    println!("{} {}", desired_roll[0], desired_roll[1]);
+    //Assert statement as sanity check.
+    //assert_eq!(desired_roll, [2,6]);
+
+    //Displaying min/max for the desired roll
+    println!("Max possible value is {}, and min possible value is {}", iterator*die, iterator);
+
+    //Initializing variables necessary for loop
+    let mut iteration = 0;
+    let mut sum = 0;
+    let mut rolls: Vec<i32> = Vec::new();
+
+    //Loop to 'roll' each die
+    loop{
+        //Incrementing the counter
+        iteration = iteration + 1;
+
+        //Rolling the die
+        let roll = rand::thread_rng()
+            .gen_range(1,10);
+
+        //Displaying each roll in case the rolls individually matter
+        println!("Roll {}: {}", iteration, roll);
+
+        //Incrementing sum by the roll
+        sum = sum + roll;
+
+        //Pushing the roll to the rolls Vec as courtesy to myself if I decide to upgrade
+        //functionality
+        rolls.push(roll);
+
+        //Break if loop counter reaches or surpasses user input
+        match iteration.cmp(&iterator) {
+            Ordering::Greater => break,
+            Ordering::Equal => break,
+            Ordering::Less => continue
+        }
+
+    }
+    //Displaying final result
+    println!("The sum was {}", sum);
 }
